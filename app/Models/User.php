a6e9f508238ac40bf55f0368e09370effe02c4c9
Naufal-Nafi <naufalabyu4869@gmail.com>
2025-06-20 01:27:06 +0700

    add notifications

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\CustomResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Notifications\CustomResetPassword;
+
 
 
 class User extends Authenticatable
@@ -57,9 +58,9 @@ class User extends Authenticatable
         return $this->hasMany(Consignment::class, 'user_id', 'user_id');
     }
 
-        public function sendPasswordResetNotification($token)
+     public function sendPasswordResetNotification($token)
     {
         $this->notify(new CustomResetPassword($token));
-    }
+    }   
 
 }
--- app/Notifications/CustomResetPassword.php
+++ app/Notifications/CustomResetPassword.php
@@ -1,10 +1,38 @@
 <?php
-use Illuminate\Auth\Notifications\ResetPassword;
-use Illuminate\Notifications\Messages\MailMessage;
 
-class CustomResetPassword extends ResetPassword
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CustomResetPassword extends Notification
 {
-    public function toMail($notifiable)
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
     {
         $url = "https://frontend-alganis-production.up.railway.app/reset-password/{$this->token}?email=" . urlencode($notifiable->getEmailForPasswordReset());
 
@@ -13,5 +41,18 @@ class CustomResetPassword extends ResetPassword
             ->line('You are receiving this email because we received a password reset request for your account.')
             ->action('Reset Password', $url)
             ->line('If you did not request a password reset, no further action is required.');
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
     }
 }
