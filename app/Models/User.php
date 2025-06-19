006bd4f41125e4da4f82c5f8dafd33ce4af4eeae
Naufal-Nafi <naufalabyu4869@gmail.com>
2025-06-20 00:11:24 +0700

    fix user.php?

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,11 +55,6 @@ class User extends Authenticatable
     public function consignments()
     {
         return $this->hasMany(Consignment::class, 'user_id', 'user_id');
-    }
-
-        public function sendPasswordResetNotification($token)
-    {
-        $this->notify(new CustomResetPassword($token));
-    }
+    }        
 
 }
