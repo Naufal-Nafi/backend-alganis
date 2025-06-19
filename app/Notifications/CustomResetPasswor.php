12feead9892d2664e5d5ba53b0513b453927318d
Naufal-Nafi <naufalabyu4869@gmail.com>
2025-06-19 23:49:44 +0700

    fix link notif?

--- app/Notifications/CustomResetPasswor.php
+++ app/Notifications/CustomResetPasswor.php
@@ -7,7 +7,7 @@ class CustomResetPassword extends ResetPassword
 {
     protected function resetUrl($notifiable)
     {
-        return "http://127.0.0.1:8080/reset-password?token={$this->token}&email=" .
+        return "https://backend-alganis-production.up.railway.app/reset-password?token={$this->token}&email=" .
                urlencode($notifiable->getEmailForPasswordReset());
     }
 }
